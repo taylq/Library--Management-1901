b46883e010a1d6d29fef1e92ffed537a3028e599
taylq <taylqmail@gmail.com>
2019-01-11 12:58:45 +0700

    user view detail book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,19 @@
 class BooksController < ApplicationController
+  before_action :find_book, except: %i(index)
+
   def index
     @books = Book.select_attr.page(params[:page]).per(Settings.book_per_page)
       .search params[:search]
     @categories = Category.select_attr
+  end
+
+  def show; end
+
+  private
+
+  def find_book
+    return if @book = Book.find_by(id: params[:id])
+    flash[:danger] = t "books.find.fail"
+    redirect_to books_path
   end
 end
