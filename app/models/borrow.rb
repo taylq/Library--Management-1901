aefc8c49dc6c4379826ffde027aace39e1e8419f
taylq <taylqmail@gmail.com>
2019-01-16 10:13:00 +0700

    admin manager request borrow book

--- app/models/borrow.rb
+++ app/models/borrow.rb
@@ -7,8 +7,29 @@ class Borrow < ApplicationRecord
 
   validates :started_at, presence: true
   validates :finished_at, presence: true
+  delegate :name, to: :user, prefix: true
+  delegate :name, to: :book, prefix: true
+
   scope :select_attr, ->{select(:id, :user_id, :book_id, :started_at,
     :finished_at, :status)}
+  scope :find_by_status, ->(book_id, finished_at){
+    where "started_at < ? and book_id = ? and status = ?", finished_at, book_id, Settings.waiting
+  }
+  scope :search_scope, ->(status, search){
+    joins(:book, :user).where(status: "#{status}")
+    .where("books.name like '%#{search}%' or users.name like '%#{search}%'")
+  }
+  scope :check_record, ->(user_id, book_id){where user_id: user_id, book_id: book_id, status: [:waiting] }
+
+  class << self
+    def search search, status
+      if search
+        search_scope search, status
+      else
+        Borrow.select_attr
+      end
+    end
+  end
 
   private
 
